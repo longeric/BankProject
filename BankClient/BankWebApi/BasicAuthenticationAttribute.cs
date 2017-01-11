e7de0ed121baa07cb7656cc7625e2e89cf208d97
Eric Yan <Tim-ofYan@hotmail.com>
2017-01-11 12:23:23 -0500

    create some new file, in web api

---
+++ BankClient/BankWebApi/BasicAuthenticationAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Security.Principal;
+
+namespace BankWebApi
+{
+    public class BasicAuthenticationAttribute : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if(actionContext.Request.Headers.Authorization == null)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+            }
+            else
+            {
+                string authenticationToken = actionContext.Request.Headers.Authorization.Parameter;
+                string decodedauthenticationToken =  Encoding.UTF8.GetString(Convert.FromBase64String(authenticationToken));
+                string[] usernamePasswordArray = decodedauthenticationToken.Split(':');
+                string username = usernamePasswordArray[0];
+                string password = usernamePasswordArray[1];
+
+                if(UserSecurity.Login(username, password))
+                {
+                    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(username), null);
+                }
+                else
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+            }
+        }
+    }
+}
---
+++ BankClient/BankWebApi/UserSecurity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataAccessLayer;
+
+namespace BankWebApi
+{
+    public class UserSecurity
+    {
+        public static bool Login(string username, string password)
+        {
+            using (bankEntities bankentity = new bankEntities())
+            {
+                return bankentity.LogIns.Any(user => user.LoginID.Equals(username) && user.Password == password);
+            }
+
+        }
+    }
+}
